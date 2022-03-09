0ac628bbc92a647fc8aded5c13240049eeda3657
prabhabalu <prabhabalu27@gmail.com>
2022-03-09 14:47:25 +0530

    Git trial

---
+++ src/test/java/GitClass.java
@@ -0,0 +1,14 @@
+
+public class GitClass {
+	private void method1() {
+System.out.println("1");
+	}
+		private void method2() {
+System.out.println("2");
+		}
+		private void method3() {
+System.out.println("3");
+		}
+	}
+
+
