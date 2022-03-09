d222658153f9f51d991fa10767125696fe895fca
prabhabalu <prabhabalu27@gmail.com>
2022-03-09 14:58:54 +0530

    push code

--- src/test/java/GitClass.java
+++ src/test/java/GitClass.java
@@ -1,14 +1,14 @@
 
 public class GitClass {
-	private void method1() {
-System.out.println("1");
-	}
-		private void method2() {
-System.out.println("2");
-		}
-		private void method3() {
-System.out.println("3");
-		}
+	public void method1() {
+		System.out.println("1");
 	}
 
+	public void method2() {
+		System.out.println("2");
+	}
 
+	public void method3() {
+		System.out.println("3");
+	}
+}
